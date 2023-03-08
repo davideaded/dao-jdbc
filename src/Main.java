3bc2b687e4896f80b86dce71d39b4925edcd1699
davideaded <david.castrodc033@gmail.com>
2023-03-08 11:07:31 -0300

    insert method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.Date;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,5 +12,21 @@ public class Main {
         System.out.println("=== TEST 1: seller findById ===== ");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println("=== TEST 2: seller findByDepartment ===== ");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+        System.out.println("=== TEST 3: seller findAll ===== ");
+        List<Seller> list2 = sellerDao.findAll();
+        for (Seller obj : list2) {
+            System.out.println(obj);
+        }
+        System.out.println("=== TEST 4: seller insert ===== ");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
