7c9d13f603b675d570541e5964aea92c454c99aa
davideaded <david.castrodc033@gmail.com>
2023-03-08 10:07:39 -0300

    Refactoring factory

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(2, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        System.out.println("=== TEST 1: seller findById ===== ");
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
